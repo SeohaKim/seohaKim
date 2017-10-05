c3e516ef3c836e06d3e70b9a82db21a9543b4f7e
Gwak Seon-geun <seon-geun@users.noreply.github.com>
2017-10-05 18:54:32 +0900

    test A

--- src/seohafont.js
+++ src/seohafont.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
-
-class seohafont extends Component {
-    render(){
-        return(
-        <div>
-        	hello
-       	</div>
-       	<div>
-       	<div>
-       		please
-       	</div>
-       	);	
-    }
+import shf_data from './seohafont_data.js';
+const canvas_style = {
+  background: 'white'
+};
+class Seohafont extends Component {
+  componentDidMount() {
+    const WIDTH = 40;
+    const HEIGHT = 80;
+    let x = shf_data['A'].vectors[0].x * 10;
+    let y = shf_data['A'].vectors[0].y * 10;
+    const ctx = this.refs.canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    shf_data['A'].vectors.slice(1).forEach(function(e) {
+      x += e.x * 10;
+      y += e.y * 10;
+      console.log(x, y);
+      ctx.lineTo(x, y);
+    });
+    ctx.lineWidth = 2;
+    ctx.stroke();
+  }
+  render() {
+    return <canvas ref="canvas" className="seoha_text" style={canvas_style} />;
+  }
 }
 
-
-
-
-
+export default Seohafont;
---
+++ src/seohafont_data.js
@@ -0,0 +1,36 @@
+const A = {
+  vectors: [
+    {
+      x: 0,
+      y: 8
+    },
+    {
+      x: 2,
+      y: -8
+    },
+    {
+      x: 2,
+      y: 8
+    },
+    {
+      x: -3,
+      y: -4
+    }
+  ]
+};
+const B = {
+  vectors: [
+    {
+      x: 0,
+      y: 0
+    },
+    {
+      x
+    }
+  ]
+};
+
+const shf_data = {
+  A: A
+};
+export default shf_data;
