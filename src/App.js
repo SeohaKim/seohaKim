73ff935224a1c63c564cb26631a6159b797d57ff
seohaKim <keynotforlock@gmail.com>
2017-10-10 13:33:41 +0900

    A-E

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Seohafont text="SEOHA KIM" />
+        <Seohafont text="ABCDEF">SEOHA KIM</Seohafont>
       </div>
     );
   }
--- src/seohafont.js
+++ src/seohafont.js
@@ -3,22 +3,12 @@ import shf_data from './seohafont_data.js';
 const canvas_style = {};
 class Seohafont extends Component {
   componentDidMount() {
-    const WIDTH = 40;
-    const HEIGHT = 80;
-    let x = shf_data['A'].vectors[0].x * WIDTH;
-    let y = shf_data['A'].vectors[0].y * HEIGHT;
     const ctx = this.refs.canvas.getContext('2d');
-
     ctx.beginPath();
     ctx.lineWidth = 1;
-    ctx.moveTo(x, y);
-    shf_data['A'].vectors.slice(1).forEach(function(e) {
-      x += e.x * WIDTH;
-      y += e.y * HEIGHT;
-      console.log(x, y);
-      ctx.lineTo(x, y);
+    this.props.text.split('').forEach((e, i) => {
+      this.renderCharacter(i * 60, 10, 50, 100, e, ctx);
     });
-
     ctx.stroke();
   }
 
@@ -34,9 +24,20 @@ class Seohafont extends Component {
    */
   renderCharacter(x, y, w, h, c, ctx) {
     if (!(c in shf_data)) return;
-    const vectors = shf_data[c].map(x => ({ x: x * w, y: y * h }));
-    let tx = x + vectors[1];
-    let ty = y + vectors[1];
+
+    const vectors = shf_data[c].vectors.map(e => ({ x: e.x * w, y: e.y * h }));
+    let tx = x + vectors[0].x;
+    let ty = y + vectors[0].y;
+    console.log(vectors);
+    console.log(x, y);
+    ctx.moveTo(tx, ty);
+    console.log(tx, ty);
+    vectors.slice(1).forEach(function(e) {
+      tx += e.x;
+      ty += e.y;
+      ctx.lineTo(tx, ty);
+      console.log(tx, ty);
+    });
   }
 
   render() {
